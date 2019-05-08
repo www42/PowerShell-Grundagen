c47c064989a9fbcd6fd2035090ef625d8b8eadcf
www42 <thomas.jaekel@gfn.de>
2019-05-08 07:03:24 +0200

    Übung 6 hinzugefügt

---
+++ Übung 6 mit Lösung.ps1
@@ -0,0 +1,34 @@
+﻿# Welche Version des .Net Frameworks ist installiert?
+#
+# Tip: HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full
+#      Property 'Release'
+#
+#      Tabelle https://msdn.microsoft.com/en-us/library/hh925568(v=vs.110)
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full'
+
+$DotNet = Get-ItemProperty -Path $RegPath -Name Release
+$Release = $DotNet.Release
+
+switch ($Release)
+{
+ "378389" {"4.5"}
+ "378675" {"4.5.1"}
+ "378758" {"4.5.1"}
+ "379893" {"4.5.2"}
+ "393295" {"4.6"}
+ "393297" {"4.6"}
+ "394254" {"4.6.1"}
+ "394271" {"4.6.1"}
+ "394802" {"4.6.2"}
+ "394806" {"4.6.2"}
+ "460798" {"4.7"}
+ "460805" {"4.7"}
+ "461308" {"4.7.1"}
+ "461310" {"4.7.1"}
+ "461808" {"4.7.2"}
+ "461814" {"4.7.2"}
+ "528040" {"4.8"}
+ "528049" {"4.8"}
+ Default  {"Unknown"}
+}
---
+++ Übung 6.ps1
@@ -0,0 +1,6 @@
+﻿# Welche Version des .Net Frameworks ist installiert?
+#
+# Tip: HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full
+#      Property 'Release'
+#
+#      Tabelle https://msdn.microsoft.com/en-us/library/hh925568(v=vs.110)
